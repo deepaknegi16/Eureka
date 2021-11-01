714b23c1c92343a1fa9b74da4898a510b662e4f7
Deepak Negi <deepaknegi.1616@gmail.com>
2021-11-01 21:51:16 +0530

    add 3 line space and a comment

--- src/main/java/com/micro/service/ServiceRegistry/controller/AppController.java
+++ src/main/java/com/micro/service/ServiceRegistry/controller/AppController.java
@@ -8,12 +8,21 @@ public class AppController {
 
     // adding a comment here.
     // this endpoint returns hello.
+
+    // let see what happens to this comment
+
     // again adding local commect
 
 
     // if i add comment here
+
     @GetMapping("/employee")
     public String getEmployee(){
         return "Hello";
+    }
+    
+    @GetMapping("/all")
+    public String AllEmployeeName() {
+        return "Employees";
     }
 }
